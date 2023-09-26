878e7aedd69213a9f632f77fc13359e6902b2305
Victor Fleury <vfleury@rodeofx.com>
2023-09-26 15:23:08 -0400

    update media keys

--- spotify.sh
+++ spotify.sh
@@ -4,8 +4,8 @@ status=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpri
 dbus_data=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata')
 
 #artist=`echo $dbus_data | sed -n '/artist/{n;p}'| cut -d '"' -f 2`
-artist=$(echo "$dbus_data" | sed -n '/artist/{n;p}' | cut -d '"' -f 2)
-#artist=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' |sed -n -e "/title/n;28p" |cut -d '"' -f 2)
+#artist=$(echo "$dbus_data" | sed -n '/artist/{n;p}' | cut -d '"' -f 2)
+artist=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' |sed -n -e "/title/n;28p" |cut -d '"' -f 2)
 echo $artist
 
 track=$(echo "$dbus_data" | sed -n '/title/{n;p}'| cut -d '"' -f 2)
