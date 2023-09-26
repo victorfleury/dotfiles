3b7f4534e1855848b092df3e3d43967501349c9e
Victor Fleury <victor.fleury@gmail.com>
2023-09-26 14:20:23 -0400

    adding poly bar scripts

---
+++ polybar/.config/polybar/scripts/vpn.sh
@@ -0,0 +1,7 @@
+status=$(nmcli -t -f name,type connection show --order name --active 2>/dev/null | grep vpn | head -1 | cut -d ':' -f 1)
+if [[ -z $status ]]; then
+    echo " Rodeo"
+else
+    echo " Rodeo"
+fi
+
