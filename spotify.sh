e1a105e15fac179f3d6e8efd041bdc23240064e5
Victor Fleury <vfleury@rodeofx.com>
2023-09-28 10:07:58 -0400

    Update configs

--- spotify.sh
+++ spotify.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/bash
 #
-status=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' |grep -o Playing)
-dbus_data=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata')
+#status=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' |grep -o Playing)
+#dbus_data=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata')
 
-#artist=`echo $dbus_data | sed -n '/artist/{n;p}'| cut -d '"' -f 2`
-#artist=$(echo "$dbus_data" | sed -n '/artist/{n;p}' | cut -d '"' -f 2)
-artist=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' |sed -n -e "/title/n;28p" |cut -d '"' -f 2)
-echo $artist
+#artist=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' |sed -n -e "/title/n;28p" |cut -d '"' -f 2)
+#track=$(echo "$dbus_data" | sed -n '/title/{n;p}'| cut -d '"' -f 2)
 
-track=$(echo "$dbus_data" | sed -n '/title/{n;p}'| cut -d '"' -f 2)
-
-echo "Status : $status"
-#echo "dbus data : $dbus_data"
-echo "Currently playing : $artist - $track"
+##echo "Status : $status"
+##echo "dbus data : $dbus_data"
+artist=$(playerctl --player=spotify metadata --format "{{ artist }}")
+title=$(playerctl --player=spotify metadata --format "{{ title }}")
+status=$(playerctl --player=spotify status)
+if [[ $status == *"Paused"* ]];then
+    icon=""
+else
+    icon=""
+fi
+echo " $icon $artist - ${title:0:25}"
