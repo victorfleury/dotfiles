7280c8e7c822e549070ff4ba2d8d4c96ebe4dd78
Victor Fleury <vfleury@rodeofx.com>
2023-09-29 14:12:10 -0400

    Update using dunst and betterlockscreen

--- polybar/.config/polybar/scripts/vpn.sh
+++ polybar/.config/polybar/scripts/vpn.sh
@@ -1,6 +1,7 @@
 status=$(nmcli -t -f name,type connection show --order name --active 2>/dev/null | grep vpn | head -1 | cut -d ':' -f 1)
 if [[ -z $status ]]; then
     echo " Rodeo"
+    notify-send -u critical VPN "VPN was disconnected"
 else
     echo " Rodeo"
 fi
